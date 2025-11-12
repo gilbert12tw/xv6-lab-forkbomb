cf38c39c586f78eaa095f515d39874a7d509649e
Gilbert Yeh <gilbert12.tw@gmail.com>
2025-11-12 07:13:12 +0000

    Testing

--- user/sh.c
+++ user/sh.c
@@ -124,9 +124,7 @@ runcmd(struct cmd *cmd)
 
   case BACK:
     bcmd = (struct backcmd*)cmd;
-    if(fork1() == 0)
-      runcmd(bcmd->cmd);
-    break;
+    runcmd(bcmd->cmd);
   }
   exit(0);
 }
@@ -140,6 +138,14 @@ getcmd(char *buf, int nbuf)
   if(buf[0] == 0) // EOF
     return -1;
   return 0;
+}
+
+void
+non_block_poll() {
+  int status;
+  int pid;
+  while ((pid = wait_noblock(&status)) > 0)
+    printf("[bg %d] exited with status %d\n", pid, status);
 }
 
 int
@@ -158,6 +164,10 @@ main(void)
 
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
+    // non blocking poll
+    non_block_poll();
+
+    // built in command here 
     if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
       // Chdir must be called by the parent, not the child.
       buf[strlen(buf)-1] = 0;  // chop \n
@@ -165,9 +175,18 @@ main(void)
         fprintf(2, "cannot cd %s\n", buf+3);
       continue;
     }
-    if(fork1() == 0)
-      runcmd(parsecmd(buf));
-    wait(0);
+
+    // LAB
+    struct cmd *cmd = parsecmd(buf);
+    int pid = fork1();
+    if(pid == 0)
+      runcmd(cmd);
+    
+    if (cmd->type != BACK)
+      wait(0);
+    else {
+      printf("[%d]\n", pid);
+    }
   }
   exit(0);
 }
